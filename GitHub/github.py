1fb6d6f90e0905a636fb1f3e7d337274dcdc918f
Anuj <anujmishra7286@gmail.com>
2021-08-02 16:55:17 +0530

    What is github

---
+++ GitHub/github.py
@@ -0,0 +1,19 @@
+ # Following are The commands to Add a project on github 
+print("Hello World")
+# 1. touch gitignore
+
+# 2. git config --global user.name "Anuj"
+
+# 3. git config --global user.email "anuj@gmail.com"
+
+# 4. git init 
+
+# 5. git add .
+
+# 6. git commit -m "Intial Commit"
+
+# 7. git status
+
+# 8. git remote add origin your_ssh_url
+
+# 9. git push origin master
